ffcd345372d181a507b2ad72b714362cd23bea47
Tohar <Toharlevi2005@gmail.com>
2026-03-22 17:00:39 +0200

    test were written for Point class

--- unittests/primitives/PointTests.java
+++ unittests/primitives/PointTests.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PointTests {
 
-    private static final double DELTA = 0.000001;
+    private static final double DELTA = 1e-6;
 
     /**
      * Test method for {@link primitives.Point#subtract(primitives.Point)}.
@@ -27,15 +27,49 @@ public class PointTests {
                 "ERROR: Point subtract(itself) does not throw an exception");
     }
 
+    //* Test method for {@link primitives.Point#add(primitives.Point)}.
+
     @Test
     void testAdd() {
+        // EP01: Simple addition of a vector to a point
+
         Point p1 = new Point(1, 2, 3);
         Vector v1 = new Vector(2, 4, 6);
         assertEquals(new Vector(3, 6, 9), p1.add(v1), "ERROR: Point add(Vector) result is wrong");
 
-//        vector
-//        // Boundary Values Tests
-//        assertThrows(IllegalArgumentException.class,()->;
+
+        // BV01: Addition of opposite vector resulting in origin (Point.ZERO)
+        Vector vOpposite = new Vector(-1, -2, -3);
+        assertEquals(Point.ZERO, p1.add(vOpposite), "ERROR: Point add(Vector) to origin result is wrong");
 
     }
+
+    // * Test method for {@link primitives.Point#distanceSquared(primitives.Point)}.
+    @Test
+    void testDistanceSquared() {
+// EP01: Simple distanceSquared of a point and a point
+        Point p1 = new Point(4, 6, 8);
+        Point p2=new Point (4,6,2);
+        assertEquals(36,p1.distanceSquared(p2),DELTA,"ERROR: Point distanceSquared(Point) to origin result is wrong" );
+
+        // BV01: distanceSquared of point with itself
+        assertEquals(0,p1.distanceSquared(p1),DELTA,"ERROR: Point distanceSquared(itself) to origin result is wrong" );
+
+    }
+
+    // * Test method for {@link primitives.Point#dintance(primitives.Point)}.
+    @Test
+    void testdistance() {
+        // EP01: Simple distanceSquared of a point and a point
+        Point p1 = new Point(4, 6, 8);
+        Point p2=new Point (2,2,2);
+        assertEquals(6,p1.distance(p2),DELTA,"ERROR: Point distance(Point) to origin result is wrong" );
+
+        // BV01: distance of point with itself
+        assertEquals(0,p1.distance(p1),DELTA,"ERROR: Point distance(itself) to origin result is wrong" );
+
+    }
+
+    }
+
 }
